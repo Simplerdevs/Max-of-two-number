9d3685ee6addd5038c75519ce815ca63268c3bf2
Simplerdevs <tplg7839@gmail.com>
2024-06-07 09:59:54 -0700

    Add project files.

---
+++ Max of two number/Max of two number.cpp
@@ -0,0 +1,38 @@
+
+						/* Program to as user to enter two number then print the max number */
+
+#include <iostream>
+using namespace std;
+									
+int main()
+
+
+{
+
+	int a, b;
+	cout << "Please enter the first number" << endl;
+	cin >> a;
+
+	cout << "Please enter the second number" << endl;
+	cin >> b;
+
+	cout << "the max is : ";
+
+
+	if (a > b) {
+
+		cout << a << endl;
+	}
+
+	else {
+
+		cout << b << endl;
+	}
+
+
+	return 0;
+
+	// created by @ilyes_trabelsi
+
+
+}
